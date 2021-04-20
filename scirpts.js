752017e1b37bc274889641133234f2b38cd74ea3
Oleksii Onyshchuk <oleksii.onyshchuk@additiv.com>
2021-04-20 20:46:04 +0300

    - style files cleanup; - add phone icon; - start styles for 900w screens

--- scirpts.js
+++ scirpts.js
@@ -1,64 +1,61 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  const navList = document.querySelector('nav ul')
-  const menuIcon = document.querySelector('.menu_icon');
-  const closeItem = document.querySelector('.nav__link');
-  
+window.addEventListener("DOMContentLoaded", (event) => {
+  const navList = document.querySelector(".nav__list");
+  const menuIcon = document.querySelector(".menu_icon");
+  const closeItem = document.querySelector(".nav__link");
+
   if (menuIcon) {
-    menuIcon.addEventListener('click', (event) => {
+    menuIcon.addEventListener("click", (event) => {
       event.preventDefault();
-      navList.classList.toggle('close');
+      navList.classList.toggle("close");
       closeItem.focus();
-    })
+    });
   }
 
   if (closeItem) {
-    closeItem.addEventListener('click', (event) => {
+    closeItem.addEventListener("click", (event) => {
       event.preventDefault();
-      navList.classList.toggle('close');
+      navList.classList.toggle("close");
       menuIcon.focus();
-    })
+    });
   }
 
-  const popup = document.querySelector('.popup');
-  const popupWrapper = document.querySelector('.popup');
-  const popupForm = document.querySelector('.popup__form');
-
-  popupForm.addEventListener('click', (event) => {
+  const popup = document.querySelector(".popup");
+  const popupWrapper = document.querySelector(".popup");
+  const popupForm = document.querySelector(".popup__form");
+  popupForm.addEventListener("click", (event) => {
     event.stopPropagation();
   });
-  popupWrapper.addEventListener('click', () => {
-    popup.classList.toggle('open');
-    document.body.classList.toggle('popuped');
+
+  popupWrapper.addEventListener("click", () => {
+    popup.classList.toggle("open");
+    document.body.classList.toggle("popuped");
   });
 });
 
+
 function submitForm(event) {
   event.preventDefault();
-  
   const form = event.target;
   const source = window.formSource;
-  
   const formData = new FormData(form);
-  const name = formData.get('name');
-  const phone = formData.get('phone');
-
-  fetch('send.php', {
-    method: 'POST',
-    body: JSON.stringify({
-      name,
-      phone,
+  const name = formData.get("name");
+  const phone = formData.get("phone");
+  fetch("send.php", {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        phone,
+      }),
     })
-  })
     .then()
     .catch();
 }
 
+
 function openPopup(event) {
   const source = event.target.dataset.source;
-  const popup = document.querySelector('.popup');
-
-  document.body.classList.toggle('popuped');
-  popup.classList.toggle('open');
+  const popup = document.querySelector(".popup");
+  document.body.classList.toggle("popuped");
+  popup.classList.toggle("open");
   window.formSource = source;
 }
-
