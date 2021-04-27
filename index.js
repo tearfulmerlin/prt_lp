03bb44c1a937f2c1e29543dc3c9e0a4f4629ca2d
elara\oleksii.onyshchuk <kY9-dLj9>
2021-04-27 07:38:48 +0300

    add secure data

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = env.process.PORT || 80;
 app.use(bodyParser.json({ limit: '0.5mb', type: 'application/json' }));
 app.use(bodyParser.urlencoded({ limit: '0.5mb', extended: false, parameterLimit: 50000 }))
 app.use(express.static('public'));
 
 
-const { botToken } = process.env;
-const sendMessageBot = async (data) => axios.post(
-  `https://api.telegram.org/${botToken}/sendMessage`, {
-    chat_id: -1001321397381,
-    parse_mode: 'Markdown',
-    text: `
+const botToken = process.env.BOT_TOKEN;
+const sendMessageBot = async(data) => axios.post(
+    `https://api.telegram.org/${botToken}/sendMessage`, {
+        chat_id: -1001321397381,
+        parse_mode: 'Markdown',
+        text: `
           *Нова заявка*
 
           Ім'я:
@@ -22,20 +22,20 @@ const sendMessageBot = async (data) => axios.post(
           Телефон:
           ${data.phone}
         `,
-  }
+    }
 );
 
 
 app.post('/lead', function(req, res) {
-  sendMessageBot(req.body)
-    .then((tgResponce) => {
-      res.json({ staus: 'ok', ...tgResponce.data });
-    }).catch((err) => {
-      res.json({ staus: 'error' })
-    });
+    sendMessageBot(req.body)
+        .then((tgResponce) => {
+            res.json({ staus: 'ok', ...tgResponce.data });
+        }).catch((err) => {
+            res.json({ staus: 'error' })
+        });
 });
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
