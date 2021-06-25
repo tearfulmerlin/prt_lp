e3825d875bd0416303497098557f4580aa3357b9
Oleksii Onyshchuk <oleksii.onyshchuk@additiv.com>
2021-06-25 12:26:21 +0300

    fix ssl redirect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect').default;
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const axios = require('axios');
@@ -25,17 +26,11 @@ const sendMessageBot = async(data) => axios.post(
 );
 
 
+app.use(sslRedirect(['production'], 301));
 app.use(bodyParser.json({ limit: '0.5mb', type: 'application/json' }));
 app.use(bodyParser.urlencoded({ limit: '0.5mb', extended: false, parameterLimit: 50000 }))
-app.use(compression());
 app.use(express.static('public'));
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`)
-  } else {
-    next();
-  }
-});
+app.use(compression());
 
 app.post('/lead', function(req, res) {
     sendMessageBot(req.body)
