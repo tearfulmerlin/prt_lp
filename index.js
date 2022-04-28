5fc7c8042e6ff5febb94ad635b0a1f19446b22a0
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2022-04-29 01:16:25 +0300

    debug tg bot

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ const sendMessageBot = async(data) => axios.post(
         parse_mode: 'Markdown',
         text: `
           *Нова заявка*
-          
-          source:
-          ${data.source}
 
           Ім'я:
           ${data.name}
