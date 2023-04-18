48c2068c09068c3c465e267f2719db175cda3832
oleksii onyshchuk <oleksii.onyshchuk@additiv.com>
2023-04-18 21:57:35 +0300

    fix server files conflict

---
+++ prt-server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect').default;
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const axios = require('axios');
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+const botToken = process.env.BOT_TOKEN;
+const chat_id = process.env.CHAT_ID;
+const sendMessageBot = async(data) => axios.post(
+    `https://api.telegram.org/${botToken}/sendMessage`, {
+        chat_id,
+        parse_mode: 'Markdown',
+        text: `
+          *Нова заявка*
+
+          Ім'я:
+          ${data.name}
+          
+          Телефон:
+          ${data.phone}
+        `,
+    }
+);
+
+app.use(sslRedirect(['production'], 301));
+app.use(bodyParser.json({ limit: '0.5mb', type: 'application/json' }));
+app.use(bodyParser.urlencoded({ limit: '0.5mb', extended: false, parameterLimit: 50000 }))
+app.use(express.static('public', {
+    maxAge: '432000000',
+}));
+app.use(compression());
+
+app.post('/lead', function(req, res) {
+    sendMessageBot(req.body)
+        .then((tgResponce) => {
+            res.json({ message: 'success' });
+        }).catch((err) => {
+        res.status(err.response.status).json({ staus: 'error' })
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
